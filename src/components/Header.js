5530a945902bf041a5094281f8f667315a90ee4d
Niyati Panchal <niyatipanchal85@gmail.com>
2023-01-06 14:41:29 +0530

    final commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-logo">Sorting Visulizer</div>
+    </div>
+  );
+}
+
+export default Header;
